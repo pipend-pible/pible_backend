ad09d78237e1625e59f79b6bfa661631b14e3849
JongSung Kim <cloud504@naver.com>
2023-05-17 04:37:11 +0900

    유저 권한 조회 querydsl 수정

--- src/main/java/com/pible/domain/user/dao/custom/impl/CustomUserRepositoryImpl.java
+++ src/main/java/com/pible/domain/user/dao/custom/impl/CustomUserRepositoryImpl.java
@@ -4,16 +4,12 @@ import com.pible.common.entity.AuthorityEntity;
 import com.pible.common.entity.UserEntity;
 import com.pible.domain.user.dao.custom.CustomUserRepository;
 import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
 
 import static com.pible.common.entity.QAuthorityEntity.authorityEntity;
-import static com.pible.common.entity.QChannelEntity.channelEntity;
-import static com.pible.common.entity.QFanartEntity.fanartEntity;
-import static com.pible.common.entity.QTagEntity.tagEntity;
 import static com.pible.common.entity.QUserAuthorityEntity.userAuthorityEntity;
 import static com.pible.common.entity.QUserEntity.userEntity;
 
@@ -29,17 +25,7 @@ public class CustomUserRepositoryImpl extends QuerydslRepositorySupport implemen
     public List<AuthorityEntity> selectUserAuthorityList(Long userId) {
         return queryFactory.select(
                         Projections.constructor(AuthorityEntity.class,
-                                channelEntity.id,
-                                channelEntity.category,
-                                fanartEntity.title,
-                                userEntity.id,
-                                userEntity.email,
-                                userEntity.nickName,
-                                fanartEntity.likeCount,
-                                fanartEntity.hitCount,
-                                Expressions.stringTemplate("string_agg({0}, {1})", tagEntity.tag.coalesce(""), ",").as("tagList"),
-                                fanartEntity.createDate,
-                                fanartEntity.id
+                                authorityEntity
                         )
                 )
                 .from(authorityEntity)
