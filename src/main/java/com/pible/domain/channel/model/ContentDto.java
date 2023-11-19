d92f460f9585e17d3baf692cd7da2f546f22214c
JongSung Kim <cloud504@naver.com>
2023-11-19 13:22:29 +0900

    주석 추가

--- src/main/java/com/pible/domain/channel/model/ContentDto.java
+++ src/main/java/com/pible/domain/channel/model/ContentDto.java
@@ -11,6 +11,10 @@ import java.util.List;
 
 @Getter
 @Setter
+// 프론트에서 Get 메소드로 요청할 경우 화면에서 보낸 프로퍼티랑 자바 변수명이랑 맞추기 위한 어노테이션입니다.
+// 내부 라이브러리로 개발되었으며 @SnakeSetter 어노테이션이 있을 경우 컴파일시 코드가 추가되어
+// setUser_id 와 같은이름의 메소드가 생성됩니다.
+// 프론트에서는 스네이크 케이스, 백엔드에서는 카멜 케이스를 쓰는 경우 매핑을 위해 추가되었습니다.
 @SnakeSetter
 @NoArgsConstructor
 @AllArgsConstructor
